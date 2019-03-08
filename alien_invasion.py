a5014880d49f44c5b4a6370836af5439822b2b80
Yan Sereda <yan.sereda@gmail.com>
2019-03-09 00:09:13 +0200

    settings move to extend class

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,18 +1,19 @@
 import sys
 import pygame
+from settings import Settings
 
 
 def run_game():
     '''Main function'''
     pygame.init() # initialization pygame library
-    screen = pygame.display.set_mode((1200, 700)) # create main window for game
+    ai_settings = Settings()
+    screen = pygame.display.set_mode((ai_settings.screen_height, ai_settings.screen_width)) # create main window for game
     pygame.display.set_caption('Alien_invasion') # set label for main window
-    bg_color = (230, 230, 230)
     while True:
         for event in pygame.event.get(): # if have any event, process cycle
             if event.type == pygame.QUIT: # stop cycle if close main window
                 sys.exit()
-        screen.fill(bg_color) # fill main window solid color
+        screen.fill(ai_settings.bg_color) # fill main window solid color
         pygame.display.flip() # redraw all object in main window
 
 
