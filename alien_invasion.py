994d035b0cb7e36b4087bbf956c41c60713e2b56
Yan Sereda <yan.sereda@gmail.com>
2019-03-08 23:38:15 +0200

    add description and set bg_color

--- alien_invasion.py
+++ alien_invasion.py
@@ -3,14 +3,17 @@ import pygame
 
 
 def run_game():
-    pygame.init()
-    screen = pygame.display.set_mode((1200, 700))
-    pygame.display.set_caption('Alien_invasion')
+    '''Main function'''
+    pygame.init() # initialization pygame library
+    screen = pygame.display.set_mode((1200, 700)) # create main window for game
+    pygame.display.set_caption('Alien_invasion') # set label for main window
+    bg_color = (230, 230, 230)
     while True:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
+        for event in pygame.event.get(): # if have any event, process cycle
+            if event.type == pygame.QUIT: # stop cycle if close main window
                 sys.exit()
-        pygame.display.flip()
+        screen.fill(bg_color) # fill main window solid color
+        pygame.display.flip() # redraw all object in main window
 
 
-run_game()
+run_game() # Call function 'run_game'
