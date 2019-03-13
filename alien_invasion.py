be4b184f85274c6dbbed9300d85adb81d232b820
Yan Sereda <yan.sereda@gmail.com>
2019-03-13 21:11:40 +0200

    add row with Aliens

--- alien_invasion.py
+++ alien_invasion.py
@@ -2,7 +2,6 @@ import pygame
 from pygame.sprite import Group
 from settings import Settings
 from ship import Ship
-from alien import Alien
 import game_functions as gf
 
 
@@ -16,14 +15,15 @@ def run_game():
     ai_settings = Settings()
     screen = pygame.display.set_mode((ai_settings.screen_width, ai_settings.screen_height)) # create main window for game
     pygame.display.set_caption('Alien_invasion') # set label for main window
-    ship = Ship(ai_settings, screen)
-    alien = Alien(ai_settings, screen)
+    ship = Ship(ai_settings, screen)K
     bullets = Group()
+    aliens = Group()
+    gf.create_fleet(ai_settings, screen, aliens)
     while True:
         gf.check_events(ai_settings, screen, ship, bullets)
         ship.update()
         gf.update_bullets(bullets)
-        gf.update_screen(ai_settings, screen, ship, alien, bullets, myfont)
+        gf.update_screen(ai_settings, screen, ship, aliens, bullets, myfont)
 
 
 run_game() # Call function 'run_game'
